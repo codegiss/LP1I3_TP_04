d790ba8f518f469b0b9bdc77223916dcfef1a8f4
codegiss <gisellesouza243@gmail.com>
2023-05-29 20:27:19 -0300

    mostrar data dd/mm/aaaa e dd/NomeMes/aaaa, definir se ano é bissexto, mostrar dias transcorridos

--- TP04.java
+++ TP04.java
@@ -92,31 +92,160 @@ class Data {
   }
 
   String mostra1() {
-    //devolver a data no formato: dd/mm/aaaa
-    // chamar data() se não tiver sido digitada
-    return "";
+    int d = retDia();
+    int m = retMes();
+    int a = retAno();
+
+    String dia = Integer.toString(d);
+    String mes = Integer.toString(m);
+    String ano = Integer.toString(a);
+    if (dia.length() == 1)
+      dia = "0" + dia;
+    if (mes.length() == 1)
+      mes = "0" + mes;
+    
+    switch(ano.length()) {
+      case 1:
+        ano = "000" + ano;
+        break;
+      case 2:
+        ano = "00" + ano;
+        break;
+      case 3:
+        ano = "0" + ano;
+        break;
+    }
+    
+    return "Data digitada: "+dia+"/"+mes+"/"+ano;
   }
 
   String mostra2() {
-    //devolver a data no formato: dd/mesPorExtenso/ano
-    // chamar data() se não tiver sido digitada
-    return "";
+    int d = retDia();
+    int m = retMes();
+    int a = retAno();
+
+    String dia = Integer.toString(d);
+    String mes = "";
+    String ano = Integer.toString(a);
+
+    if (dia.length() == 1)
+      dia = "0" + dia;
+
+    switch(m) {
+      case 1:
+        mes = "janeiro";
+        break;
+      case 2:
+        mes = "fevereiro";
+        break;
+      case 3:
+        mes = "março";
+        break;
+      case 4:
+        mes = "abril";
+        break;
+      case 5:
+        mes = "maio";
+        break;
+      case 6:
+        mes = "junho";
+        break;
+      case 7:
+        mes = "julho";
+        break;
+      case 8:
+        mes = "agosto";
+        break;
+      case 9:
+        mes = "setembro";
+        break;
+      case 10:
+        mes = "outubro";
+        break;
+      case 11:
+        mes = "novembro";
+        break;
+      default:
+        mes = "dezembro";
+        break;
+    }
+
+    switch (ano.length()) {
+      case 1:
+        ano = "000" + ano;
+        break;
+      case 2:
+        ano = "00" + ano;
+        break;
+      case 3:
+        ano = "0" + ano;
+        break;
+    }
+    
+    return "Data digitada: "+dia+"/"+mes+"/"+ano;
   }
 
   boolean bissexto() {
-    //devolver um boolean informando se o ano é ou não bissexto
-    return true;
+    int ano = retAno();
+    if((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0)
+      return true;
+    return false;
   }
 
   int diasTranscorridos() {
-    //retornar a quantidade de dias transcorridos no ano até a data digitada.
-    return 0;
+    int mes = retMes();
+    int dia = retDia();
+    int dias=0;
+
+    switch(mes) {
+      case 1:
+        dias = dia;
+        break;
+      case 2:
+        dias = dia + 31;
+        break;
+      case 3:
+        dias = dia + 59;
+        break;
+      case 4:
+        dias = dia + 90;
+        break;
+      case 5:
+        dias = dia + 120;
+        break;
+      case 6:
+        dias = dia + 151;
+        break;
+      case 7:
+        dias = dia + 181;
+        break;
+      case 8:
+        dias = dia + 212;
+        break;
+      case 9:
+        dias = dia + 243;
+        break;
+      case 10:
+        dias = dia + 273;
+        break;
+      case 11:
+        dias = dia + 304;
+        break;
+      case 12:
+        dias = dia + 334;
+        break;
+    }
+
+    if (bissexto() && (mes>2 || (mes==2 && dia==29)))
+      dias++;
+
+    return dias;
   }
 
   void apresentaDataAtual () {
-    //imprimir a data atual, utilizando as classes Date e DateFormat, o DateFormat empregando o seguinte método: getDateInstance(DateFormat.FULL);
-    Date dataAtual = new Date();
-    System.out.println("Data atual: ");
+    DateFormat dataAtual = DateFormat.getDateInstance();
+    String data = dataAtual.format(new Date());
+    System.out.println("Data atual: "+ data);
   }
 }
 
@@ -135,13 +264,14 @@ public class TP04 {
             System.out.println("3 - Mostrar data no formato dd/NomeMês/aaa");
             System.out.println("4 - Mostrar dias transcorridos no ano");
             System.out.println("5 - Mostrar data atual");
+            System.out.println("6 - Sair");
             System.out.print("Opção escolhida: ");
             opcao = ler.nextInt();
           } catch (Exception e) {
-            System.out.println("Escolha uma opção de 1 a 5.");
+            System.out.println("Escolha uma opção de 1 a 6.");
             ler.nextLine();
           }
-        } while (opcao<1 || opcao>5);
+        } while (opcao<1 || opcao>6);
   
         switch(opcao) {
           case 1:
@@ -159,7 +289,11 @@ public class TP04 {
           case 5:
             tp.apresentaDataAtual();
             break;
+          default:
+            break;
         }
-      } while(opcao!=5);
+      } while(opcao!=6);
+
+      ler.close();
     }
 }
